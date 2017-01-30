294f1a963d70d09b033e43e8b34d294468f12daf
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-01-29 19:52:13 -0800

    portfolio project edits #2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,35 @@
 // sanity check
-console.log("app.js linked.");
+//console.log("app.js linked.");
+
+//$(document).on('ready', function(){
+
+//});
+
+
+
+
+
+var data = {
+  "projects" :
+  [{
+    "title"       : "memory game",
+    "description" : "a game to test your memory",
+    "category"    : "web development",
+    "url"         : "file:///Users/yvonnetsu/Documents/BOOTCAMP/GeneralAssembly/Portfolio/tsuyy.github.io/projects/memory-game/memory-game.html",
+    "img"         : "http://cliparts.co/cliparts/Acb/jGk/AcbjGkLc4.jpg"
+  },
+  {
+    "title"       : "mr. fox",
+    "description" : "practicing with json",
+    "category"    : "web development",
+    "url"         : "file:///Users/yvonnetsu/Documents/BOOTCAMP/GeneralAssembly/Portfolio/tsuyy.github.io/projects/json-to-html-with-mr-fox/index.html",
+    "img"         : "https://media.giphy.com/media/aiL0BGeWHbtRu/giphy.gif"
+  },
+  {
+    "title"       : "gradients",
+    "description" : "colors of the sky",
+    "category"    : "photography",
+    "url"         : "",
+    "img"         : ""
+  }]
+}
