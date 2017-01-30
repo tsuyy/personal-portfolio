d8ce604b576e5347cb1737990d0541cfaef99b9f
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-01-29 21:22:40 -0800

    first portfolio project submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,15 @@
 // sanity check
 //console.log("app.js linked.");
 
-//$(document).on('ready', function(){
-
-//});
 
 
+$(document).ready(function(){
+  for(var i = 0; i < data.projects.length; i++) {
+    var projects = data.projects[i];
+    var item = ('<li>' +  projects.title + ' : ' + projects.category + '</li>');
+    $('ul.projects').append(item);
+  };
+});
 
 
 
@@ -16,7 +20,7 @@ var data = {
     "description" : "a game to test your memory",
     "category"    : "web development",
     "url"         : "file:///Users/yvonnetsu/Documents/BOOTCAMP/GeneralAssembly/Portfolio/tsuyy.github.io/projects/memory-game/memory-game.html",
-    "img"         : "http://cliparts.co/cliparts/Acb/jGk/AcbjGkLc4.jpg"
+    "img"         : "http://www.clipartkid.com/images/126/poker-cards-chips-table-scenes-poker-tournament-scenes-poker-game-wP4CDw-clipart.png"
   },
   {
     "title"       : "mr. fox",
@@ -29,7 +33,7 @@ var data = {
     "title"       : "gradients",
     "description" : "colors of the sky",
     "category"    : "photography",
-    "url"         : "",
-    "img"         : ""
+    "url"         : "file:///Users/yvonnetsu/Documents/BOOTCAMP/GeneralAssembly/Portfolio/tsuyy.github.io/projects/photography/p.html",
+    "img"         : "projects/photography/01.jpg"
   }]
 }
